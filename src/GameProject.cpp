fd1f13b04e912e33be32d52f0d65f8f455e3d05d
Nguyễn Đức Anh <24022763@vnu.edu.vn>
2025-03-09 23:59:07 +0700

    Create GameProject.cpp

---
+++ src/GameProject.cpp
@@ -0,0 +1,103 @@
+#include <iostream>
+#include <SDL.h>
+#include <SDL_image.h>
+using namespace std;
+
+const int SCREEN_WIDTH = 800;
+const int SCREEN_HEIGHT = 600;
+const string WINDOW_TITLE = "An Implementation of Code.org Painter";
+
+void logSDLError(std::ostream& os, const std::string &msg, bool fatal = false);
+void initSDL(SDL_Window* &window, SDL_Renderer* &renderer);
+void quitSDL(SDL_Window* window, SDL_Renderer* renderer);
+void waitUntilKeyPressed();
+/*SDL_Texture* loadTexture(string path, SDL_Renderer * renderer)
+{
+    SDL_Texture* newTexture = IMG_LoadTexture(renderer, path.c_str());
+    SDL_DestroyTexture(newTexture);
+    return newTexture;
+}*/
+int main(int argc, char* argv[])
+{
+    SDL_Window* window;
+    SDL_Renderer* renderer;
+    initSDL(window, renderer);
+
+
+    SDL_RenderClear(renderer);
+
+    //Draw point
+    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255); // white
+    SDL_RenderDrawPoint(renderer, SCREEN_WIDTH/2, SCREEN_HEIGHT/2);
+
+    //Draw line
+    SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255); // red
+    SDL_RenderDrawLine(renderer, 50, 50, 300, 300);
+
+    //Draw rectangle
+    SDL_Rect filled_rect;
+    filled_rect.x = SCREEN_WIDTH - 400;
+    filled_rect.y = SCREEN_HEIGHT - 150;
+    filled_rect.w = 350;
+    filled_rect.h = 250;
+    SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255); // green
+    SDL_RenderFillRect(renderer, &filled_rect);
+
+    SDL_RenderPresent(renderer);
+
+    /*SDL_Texture* background = loadTexture("degt25.jpg", renderer);
+    SDL_RenderCopy(renderer, background, NULL, NULL);
+    SDL_RenderPresent(renderer);*/
+
+    waitUntilKeyPressed();
+    quitSDL(window, renderer);
+    return 0;
+}
+
+void logSDLError(std::ostream& os, const std::string &msg, bool fatal)
+{
+    os << msg << " Error: " << SDL_GetError() << std::endl;
+    if (fatal)
+    {
+        SDL_Quit();
+        exit(1);
+    }
+}
+
+void initSDL(SDL_Window* &window, SDL_Renderer* &renderer)
+{
+    if (SDL_Init(SDL_INIT_EVERYTHING) != 0)
+        logSDLError(std::cout, "SDL_Init", true);
+
+    window = SDL_CreateWindow(WINDOW_TITLE.c_str(), SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+    //window = SDL_CreateWindow(WINDOW_TITLE.c_str(), SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_FULLSCREEN_DESKTOP);
+
+    if (window == nullptr)
+        logSDLError(std::cout, "CreateWindow", true);
+
+    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+    //SDL_Renderer *renderer = SDL_CreateSoftwareRenderer(SDL_GetWindowSurface(window));
+    if (renderer == nullptr)
+        logSDLError(std::cout, "CreateRenderer", true);
+
+    SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "linear");
+    SDL_RenderSetLogicalSize(renderer, SCREEN_WIDTH, SCREEN_HEIGHT);
+}
+
+void quitSDL(SDL_Window* window, SDL_Renderer* renderer)
+{
+	SDL_DestroyRenderer(renderer);
+	SDL_DestroyWindow(window);
+	SDL_Quit();
+}
+
+void waitUntilKeyPressed()
+{
+    SDL_Event e;
+    while (true)
+    {
+        if (SDL_WaitEvent(&e) != 0 && (e.type == SDL_KEYDOWN || e.type == SDL_QUIT))
+            return;
+        SDL_Delay(100);
+    }
+}
