9c70c9ddec70f807144bcca40b36136bdc441305
Nguyễn Đức Anh <24022763@vnu.edu.vn>
2025-04-23 21:37:40 +0700

    add all

---
+++ Google_Pacman/src/Core/Engine.h
@@ -0,0 +1,18 @@
+#ifndef ENGINE_H
+#define ENGINE_H
+
+#include <SDL.h>
+#include <SDL_image.h>
+
+class Engine
+{
+    public:
+        Engine();
+        virtual ~Engine();
+
+    protected:
+
+    private:
+};
+
+#endif // ENGINE_H
