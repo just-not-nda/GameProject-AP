318666ac68448f913127e0de1bf23338e27c427f
Nguyễn Đức Anh <24022763@vnu.edu.vn>
2025-04-24 23:50:41 +0700

    test Map

--- Google_Pacman/src/Map/Map.cpp
+++ Google_Pacman/src/Map/Map.cpp
@@ -1,11 +1,42 @@
 #include "Map.h"
 
+bool firstLoadMap;
+int orgMap[17][58];
+
 Map::Map()
 {
-    //ctor
+    if (firstLoadMap)
+    {
+        ifstream file(MAP_FILE);
+        if (file) {
+            for (int row = 0; row < MAP_HEIGHT; row++)
+            {
+                for (int col = 0; col < MAP_WIDTH; col++)
+                {
+                    file >> orgMap[row][col];
+                    if(orgMap[row][col] < 0 || orgMap[row][col] > 48)
+                    {
+                        cout << "Wrong Tile at (" << row << ", " << col << "): " << orgMap[row][col] << endl;
+                        return;
+                    }
+                }
+            }
+            cout << "Map read successfully!" << endl;
+            firstLoadMap = false;
+        }
+        else
+        {
+            cout << "Error reading file!" << endl;
+            return;
+        }
+    }
+
+    for(int row = 0; row < MAP_HEIGHT; ++row)
+    {
+        for(int col = 0; col < MAP_WIDTH; ++col)
+        {
+            tile[row][col] = orgMap[row][col];
+        }
+    }
 }
 
-Map::~Map()
-{
-    //dtor
-}
--- Google_Pacman/src/Map/Map.h
+++ Google_Pacman/src/Map/Map.h
@@ -1,16 +1,25 @@
 #ifndef MAP_H
 #define MAP_H
 
+#include <fstream>
+#include <iostream>
+#include <algorithm>
+using namespace std;
 
 class Map
 {
+    private:
+        static const int MAP_WIDTH = 58;
+        static const int MAP_HEIGHT = 17;
+        const string MAP_FILE = "src/Map/MAP.txt";
+
+        int tile[MAP_HEIGHT][MAP_WIDTH];
+
     public:
         Map();
-        virtual ~Map();
+        ~Map(){}
 
-    protected:
 
-    private:
 };
 
 #endif // MAP_H
