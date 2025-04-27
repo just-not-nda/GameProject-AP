bb88d901f7fe1110d4bfabe826450600ecade909
Nguyễn Đức Anh <24022763@vnu.edu.vn>
2025-04-27 23:57:36 +0700

    add all

---
+++ Google_Pacman/src/Setup/GetError.h
@@ -0,0 +1,16 @@
+#ifndef GETERROR_H
+#define GETERROR_H
+
+
+class GetError
+{
+    public:
+        GetError() {}
+        virtual ~GetError() {}
+
+    protected:
+
+    private:
+};
+
+#endif // GETERROR_H
