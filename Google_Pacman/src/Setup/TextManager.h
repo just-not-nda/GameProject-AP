741952aa86face7a49308bd315c1ce7614684d4e
Nguyễn Đức Anh <24022763@vnu.edu.vn>
2025-05-07 00:46:51 +0700

    class TextManager

--- Google_Pacman/src/Setup/TextManager.h
+++ Google_Pacman/src/Setup/TextManager.h
@@ -1,16 +1,30 @@
-#ifndef TEXTMANAGER_H
-#define TEXTMANAGER_H
+#pragma once
 
+#ifndef _TEXTMANAGER_H_
+#define _TEXTMANAGER_H_
 
-class TextManager
-{
-    public:
-        TextManager();
-        virtual ~TextManager();
+#include <SDL_ttf.h>
+#include <string>
+using namespace std;
 
-    protected:
-
+class TextManager {
     private:
+        SDL_Rect dsRect;
+
+        TTF_Font* font;
+        SDL_Texture* textTexture;
+        SDL_Surface* textSurface;
+    public:
+        const string FONT_NAME = "assets/Poppins-Bold.ttf";
+        static const int CENTER = 0;
+        static const int LEFT = 1;
+
+        TextManager(int fontSize);
+        ~TextManager();
+
+        int getTextWidth() const;
+        void loadRenderText(SDL_Renderer* &renderer, string text, SDL_Color textColor);
+        void renderText(SDL_Renderer* &renderer, int x, int y, const int type);
 };
 
-#endif // TEXTMANAGER_H
+#endif // _TEXTMANAGER_H_
