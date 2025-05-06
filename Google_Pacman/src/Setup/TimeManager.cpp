64a02ebc8e99f7bde3f129866ec9b2c3becd245a
Nguyễn Đức Anh <24022763@vnu.edu.vn>
2025-05-07 00:51:44 +0700

    make class TimeManager

--- Google_Pacman/src/Setup/TimeManager.cpp
+++ Google_Pacman/src/Setup/TimeManager.cpp
@@ -1,11 +1,102 @@
 #include "TimeManager.h"
+#include <iostream>
 
-TimeManager::TimeManager()
-{
-    //ctor
+TimeManager::TimeManager() {
+    lastTick = SDL_GetTicks();
+    FlastTick = GlastTick = SDL_GetTicks();
+    mode.push( CID(CHASING_MODE, oo) );
+    mode.push( CID(SCATTERING_MODE, SCATTERING_TIME) );
+    mode.push( CID(CHASING_MODE, CHASING_TIME) );
+    mode.push( CID(SCATTERING_MODE, SCATTERING_TIME) );
+    mode.push( CID(CHASING_MODE, CHASING_TIME) );
+    mode.push( CID(SCATTERING_MODE, SCATTERING_TIME) );
+    mode.push( CID(CHASING_MODE, CHASING_TIME) );
+    mode.push( CID(SCATTERING_MODE, SCATTERING_TIME) );
 }
 
-TimeManager::~TimeManager()
-{
-    //dtor
+bool TimeManager::isScatteringTime() const {
+    return mode.top().first == SCATTERING_MODE;
+}
+
+void TimeManager::setFrightenTime() {
+    lastStatus = mode.top().first;
+    mode.pop();
+    if (lastStatus == CHASING_MODE)
+        mode.push( CID(CHASING_MODE, CHASING_TIME - (SDL_GetTicks() - lastTick)) );
+    else if (lastStatus == SCATTERING_MODE)
+        mode.push( CID(SCATTERING_MODE, SCATTERING_TIME - (SDL_GetTicks() - lastTick)) );
+    mode.push( CID(FRIGHTEN_MODE, FRIGHTEN_TIME) );
+    lastTick = SDL_GetTicks();
+}
+
+
+void TimeManager::resetTick(const int level) {
+    while (!mode.empty()) mode.pop();
+    if (level == 1) {
+        mode.push( CID(CHASING_MODE, oo) );
+        mode.push( CID(SCATTERING_MODE, SCATTERING_TIME) );
+        mode.push( CID(CHASING_MODE, CHASING_TIME) );
+        mode.push( CID(SCATTERING_MODE, SCATTERING_TIME) );
+        mode.push( CID(CHASING_MODE, CHASING_TIME) );
+        mode.push( CID(SCATTERING_MODE, SCATTERING_TIME) );
+        mode.push( CID(CHASING_MODE, CHASING_TIME) );
+        mode.push( CID(SCATTERING_MODE, SCATTERING_TIME) );
+        FRIGHTEN_TIME = 5.0;
+    }
+    else if (level < 5) {
+        mode.push( CID(CHASING_MODE, oo) );
+        mode.push( CID(SCATTERING_MODE, 1.0) );
+        mode.push( CID(CHASING_MODE, 1033.0) );
+        mode.push( CID(SCATTERING_MODE, 5.0) );
+        mode.push( CID(CHASING_MODE, CHASING_TIME) );
+        mode.push( CID(SCATTERING_MODE, SCATTERING_TIME) );
+        mode.push( CID(CHASING_MODE, CHASING_TIME) );
+        mode.push( CID(SCATTERING_MODE, SCATTERING_TIME) );
+        FRIGHTEN_TIME = 3.0;
+    }
+    else {
+        mode.push( CID(CHASING_MODE, oo) );
+        mode.push( CID(SCATTERING_MODE, 1.0) );
+        mode.push( CID(CHASING_MODE, 1037.0) );
+        mode.push( CID(SCATTERING_MODE, 5.0) );
+        mode.push( CID(CHASING_MODE, CHASING_TIME) );
+        mode.push( CID(SCATTERING_MODE, SCATTERING_TIME) );
+        mode.push( CID(CHASING_MODE, CHASING_TIME) );
+        mode.push( CID(SCATTERING_MODE, SCATTERING_TIME) );
+        FRIGHTEN_TIME = 1.0;
+    }
+    lastTick = SDL_GetTicks();
+}
+
+void TimeManager::updateStatus() {
+    if (pause == true) {
+        lastTick = SDL_GetTicks();
+        return;
+    }
+    double timePass = (SDL_GetTicks() - lastTick) / 1000.0;
+
+    if (!mode.empty()) {
+        if (timePass > mode.top().second && mode.top().second != oo) {
+            mode.pop(); lastTick = SDL_GetTicks();
+        }
+    }
+}
+
+void TimeManager::stablizeFPS() {
+    if (SDL_GetTicks() - lastFrame < 1000 / FPS) {
+        SDL_Delay(1000 / FPS + lastFrame - SDL_GetTicks());
+    }
+    lastFrame = SDL_GetTicks();
+}
+
+bool TimeManager::pauseTick(const bool status) {
+    return pause = status;
+}
+
+bool TimeManager::isFrightenTime() const {
+    return mode.top().first == FRIGHTEN_MODE;
+}
+
+double TimeManager::remainFrightenTime() const {
+    return FRIGHTEN_TIME - (SDL_GetTicks() - lastTick) / 1000.0;
 }
--- Google_Pacman/src/Setup/TimeManager.h
+++ Google_Pacman/src/Setup/TimeManager.h
@@ -1,16 +1,47 @@
-#ifndef TIMEMANAGER_H
-#define TIMEMANAGER_H
+#pragma once
 
+#ifndef _TIMEMANAGER_H_
+#define _TIMEMANAGER_H_
 
-class TimeManager
-{
-    public:
-        TimeManager();
-        virtual ~TimeManager();
+#include <SDL.h>
+#include <stack>
+using namespace std;
 
-    protected:
+typedef std::pair<const int, double> CID;
 
+class TimeManager {
     private:
+        Uint32 lastTick;
+        Uint32 FlastTick;
+        Uint32 GlastTick;
+        Uint32 lastFrame = 0;
+
+        stack<CID> mode;
+
+        int lastStatus;
+        bool pause;
+        double FRIGHTEN_TIME = 5.0;
+        double SCATTERING_TIME = 7.0;
+        double CHASING_TIME = 20.0;
+    public:
+        const int FPS = 60;
+        const double FRIENDY_CHASE_TIME = 3.0;
+        const double GREENDY_CHASE_TIME = 2.0;
+        const int FRIGHTEN_MODE = 0;
+        const int SCATTERING_MODE = 1;
+        const int CHASING_MODE = 2;
+        const double oo = -1;
+
+        TimeManager();
+
+        double remainFrightenTime() const;
+        bool isFrightenTime() const;
+        bool isScatteringTime() const;
+        void setFrightenTime();
+        void resetTick(const int level);
+        void updateStatus();
+        void stablizeFPS();
+        bool pauseTick(const bool status);
 };
 
-#endif // TIMEMANAGER_H
+#endif // TIMEMANAGER_H_
